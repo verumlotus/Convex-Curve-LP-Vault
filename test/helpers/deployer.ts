20deb84a8fa2dafc23f381706b919630e458a3f5
verumlotus <universumtremendum@gmail.com>
2022-03-24 12:42:18 -0700

    Add ConvexFixture interface, connect to all address in cvx/crv ecosystem needed in loadConvexFixture

--- test/helpers/deployer.ts
+++ test/helpers/deployer.ts
@@ -33,6 +33,15 @@ import data from "../../artifacts/contracts/Tranche.sol/Tranche.json";
 import { CompoundAssetProxy__factory } from "typechain/factories/CompoundAssetProxy__factory";
 import { CTokenInterface__factory } from "typechain/factories/CTokenInterface__factory";
 import { CompoundAssetProxy } from "typechain/CompoundAssetProxy";
+import { ConvexAssetProxy } from "typechain/ConvexAssetProxy";
+import { IConvexBooster } from "typechain/IConvexBooster";
+import { IConvexBaseRewardPool } from "typechain/IConvexBaseRewardPool";
+import { ISwapRouter } from "typechain/ISwapRouter";
+import { I3CurvePoolDepositZap } from "typechain/I3CurvePoolDepositZap";
+import { IConvexBooster__factory } from "typechain/factories/IConvexBooster__factory";
+import { IConvexBaseRewardPool__factory } from "typechain/factories/IConvexBaseRewardPool__factory";
+import { I3CurvePoolDepositZap__factory } from "./../../typechain/factories/I3CurvePoolDepositZap__factory";
+
 import { CTokenInterface } from "typechain/CTokenInterface";
 
 export interface FixtureInterface {
@@ -52,6 +61,22 @@ export interface CFixtureInterface {
   cusdc: CTokenInterface;
   usdc: IERC20;
   comp: IERC20;
+  proxy: TestUserProxy;
+}
+
+export interface ConvexFixtureInterface {
+  signer: Signer;
+  position: ConvexAssetProxy;
+  booster: IConvexBooster;
+  rewardsContract: IConvexBaseRewardPool;
+  curveZap: I3CurvePoolDepositZap;
+  curveMetaPool: string;
+  convexDepositToken: IERC20;
+  lpToken: IERC20;
+  router: ISwapRouter;
+  usdc: IERC20;
+  crv: IERC20;
+  cvx: IERC20;
   proxy: TestUserProxy;
 }
 
@@ -301,6 +326,39 @@ export async function loadCFixture(signer: Signer) {
   );
 
   return { signer, position, cusdc, usdc, comp, proxy };
+}
+
+export async function loadConvexFixture(signer: Signer) {
+  // Some addresses specific to LUSD3CRV pool
+  const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  const owner = signer;
+  const usdcAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+  const crvAddress = "0xD533a949740bb3306d119CC777fa900bA034cd52";
+  const cvxAddress = "0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B";
+  const boosterAddress = "0xF403C135812408BFbE8713b5A23a04b3D48AAE31";
+  const rewardsContractAddress = "0x2ad92A7aE036a038ff02B96c88de868ddf3f8190";
+  const pool3CrvDepositZapAddress =
+    "0xA79828DF1850E8a3A3064576f380D90aECDD3359";
+  // Metapool for LUSD3CRV, also the LP token address
+  const curveLusd3CRVPoolAddress = "0xEd279fDD11cA84bEef15AF5D39BB4d4bEE23F0cA";
+  const cvxLusd3CRV = "0xFB9B2f06FDb404Fd3E2278E9A9edc8f252F273d0";
+
+  const usdc = IERC20__factory.connect(usdcAddress, owner);
+  const crv = IERC20__factory.connect(crvAddress, owner);
+  const cvx = IERC20__factory.connect(cvxAddress, owner);
+  const booster = IConvexBooster__factory.connect(boosterAddress, owner);
+  const rewardsContract = IConvexBaseRewardPool__factory.connect(
+    rewardsContractAddress,
+    owner
+  );
+  const curveZap = I3CurvePoolDepositZap__factory.connect(
+    pool3CrvDepositZapAddress,
+    owner
+  );
+  const convexDepositToken = IERC20__factory.connect(cvxLusd3CRV, owner);
+  const lpToken = IERC20__factory.connect(curveLusd3CRVPoolAddress, owner);
+
+  const ownerAddress = await signer.getAddress();
 }
 
 export async function loadEthPoolMainnetFixture() {
