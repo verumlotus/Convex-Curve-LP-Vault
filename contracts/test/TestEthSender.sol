de9b5452b1f6de8397598de24692b3889fdc0517
verumlotus <universumtremendum@gmail.com>
2022-03-25 11:32:00 -0700

    Clean up

--- contracts/test/TestEthSender.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.0;
-
-contract TestEthSender {
-    function sendEth(address payable to) external payable {
-        to.transfer(msg.value);
-    }
-}
